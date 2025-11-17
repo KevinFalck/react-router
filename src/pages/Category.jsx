622112f85cb6b2b34d97cf9930cba0de5fd42e1d
KevinFalck <kevin.falck@gmail.com>
2025-11-17 15:29:13 +0000

    Refactor: Replace div with fragment in Category component for cleaner markup

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -3,9 +3,9 @@ import { useParams } from "react-router";
 const Category = () => {
   const { slug } = useParams();
   return (
-    <div>
+    <>
       <h2 className="my-3">Détails de la catégorie : {slug}</h2>
-    </div>
+    </>
   );
 };
 
