72ac4005dd1cc1a36eae20ca2ca743070f99ef17
KevinFalck <kevin.falck@gmail.com>
2025-11-17 16:16:17 +0000

    feat: add navigation button to return to homepage in Category component

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,10 +1,19 @@
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 
 const Category = () => {
   const { slug } = useParams();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
     <>
       <h2>Détails de la catégorie : {slug}</h2>
+      <button onClick={handleClick} className="btn btn-primary mt-3">
+        Retour à la page d'accueil
+      </button>
     </>
   );
 };
