81881b6482d5201a23b87f7904b1d6b14a6d74a7
KevinFalck <kevin.falck@gmail.com>
2025-11-17 14:44:08 +0000

    Refactor: Enhance Header component with Navlink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,37 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import "./Header.css";
 
 function Header() {
   return (
-    <header className="bg-light p-3">
-      <nav className="d-flex justify-content-end">
-        <Link to="/" className="text-decoration-none text-dark me-3">
-          Accueil
-        </Link>
-        <Link
-          to="/categorie/vetements"
-          className="text-decoration-none text-dark me-3"
-        >
-          Vêtements
-        </Link>
-        <Link
-          to="/categorie/chaussures"
-          className="text-decoration-none text-dark"
-        >
-          Chaussures
-        </Link>
-      </nav>
-    </header>
+    <Navbar bg="light" expand="lg" className="p-3">
+      <Container fluid>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="/" className="text-dark">
+              Accueil
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to="/categorie/vetements"
+              className="text-dark"
+            >
+              Vêtements
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to="/categorie/chaussures"
+              className="text-dark"
+            >
+              Chaussures
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 export default Header;
